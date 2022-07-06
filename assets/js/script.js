8d7040a21bddb44d8fe45a584f465a7a91852ee4
vyang14 <vyang014888@gmail.com>
2022-07-06 14:15:59 -0400

    fixed css link in index.html and added fetch request to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var dataStuff;
+var dataTwo;
+
+fetch("https://stormy-cliffs-87695.herokuapp.com/https://developer.nps.gov/api/v1/activities/parks?stateCode=GA&id=BFF8C027-7C8F-480B-A5F8-CD8CE490BFBA&api_key=M6zybiN7mrDQd0ocy5tTpMmFxZQmFdHcHCZZ1X0M", {  headers: {
+      Accept: "application/json",
+  }
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    dataStuff = data;
+    console.log(dataStuff);
+    console.log(dataStuff.data);
+  });
