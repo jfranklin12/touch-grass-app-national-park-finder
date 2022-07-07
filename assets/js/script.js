d36dc7ce692762a5427921d659a1ca387c9dab5c
jfranklin12 <j.franks725@gmail.com>
2022-07-07 16:27:36 -0400

    console log of lat and lon and new API set with more values

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ function showResults(pikachu) {
   searchResultsEl.innerHTML= '';
   var input = searchInputEl.value;
   resultTextEl.textContent = input;
-
+// Sets a loop to pull all of the parks that match the state code
   for( i=0; i<pikachu.length; i++){
     var resultCardEl = document.createElement('div');
     var resultMainEl = document.createElement('div');
@@ -41,11 +41,28 @@ function showResults(pikachu) {
   resultTitleEl.innerText = pikachu[i].fullName;
   resultContentEl.textContent = pikachu[i].url;
 
+  parkName = pikachu[i].fullName;
+
+  resultTitleEl.dataset.lat=pikachu[i].latitude
+  resultTitleEl.dataset.lon=pikachu[i].longitude
+  resultTitleEl.addEventListener('click', searchMap);
+  resultTitleEl.id = 'parkName';
+  resultContentEl.setAttribute("href", pikachu[i].url, '_blank');
+
   searchResultsEl.appendChild(resultCardEl);
   resultCardEl.appendChild(resultMainEl);
   resultMainEl.appendChild(resultTitleEl);
   resultMainEl.appendChild(resultContentEl);
   }
+}
+// Searches map for state park
+function searchMap(event){
+  console.log(event.target);
+  // create function for searching location on map
+  var lat = event.target.dataset.lat
+  var lon = event.target.dataset.lon
+  console.log(lat);
+  console.log(lon);
 }
 
 // Search button listener to search API for results that match User Input
@@ -53,7 +70,7 @@ searchButtonEl.addEventListener('click',function(){
     var input = searchInputEl.value;
     console.log(input);
 
-    fetch("https://developer.nps.gov/api/v1/activities/parks?id=BFF8C027-7C8F-480B-A5F8-CD8CE490BFBA,GA&api_key=M6zybiN7mrDQd0ocy5tTpMmFxZQmFdHcHCZZ1X0M", {  headers: {
+    fetch("https://developer.nps.gov/api/v1/parks?limit=500&api_key=M6zybiN7mrDQd0ocy5tTpMmFxZQmFdHcHCZZ1X0M", {  headers: {
       Accept: "application/json",
     }
   })
@@ -64,7 +81,7 @@ searchButtonEl.addEventListener('click',function(){
     console.log(data);
     // console.log(dataStuff);
     // call function getData(data)
-    var pikachu = data.data[0].parks.filter(function(park){
+    var pikachu = data.data.filter(function(park){
       return park.states.includes(input.toUpperCase())
     })
     console.log(pikachu);
